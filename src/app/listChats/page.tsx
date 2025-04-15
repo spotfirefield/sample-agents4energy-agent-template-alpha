05760b9bc9c513b5608275d7b0c16200cd6598bb
waltmayf <126521508+waltmayf@users.noreply.github.com>
2025-04-15 09:15:23 -0500

    UI (#26)
    * Working on workshop
    
    * Added a few labs
    
    * minor edits
    
    * Working on labs
    
    * Added message rendering in ipynb file
    
    * Added esm hook to allow for importing esm modules in ijs
    
    * Working on lab
    
    * Refactored message component types out of the component which picks which to render
    
    * Working on lab
    
    * Modified the calculator piece of the lab
    
    * Updated helper functions
    
    * Updated prompting
    
    * Updated lab and pySpark tool
    
    * Improved file rendering
    
    * Improved plot rendering
    
    * Improved plot loading
    
    * Cleanup
    
    * Fixed build errors
    
    * Cleanup
    
    * De-duplicated athenaWorkgroup name
    
    * Working on permissions
    
    * Added web browsing and fixed PySpark session creation logic.
    
    * Improved file handeling
    
    * Updating table rendering
    
    * Working on file handeling and pyspark
    
    * Improved plot embedding
    
    * Updated interaction with code
    
    * Improved file previews and the PySpark tool
    
    * Resolved merge conflicts
    
    * Improved file serving
    
    * Now only listing the user's chat sesions

--- src/app/listChats/page.tsx
+++ src/app/listChats/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useEffect, useState } from 'react';
 // import { stringify } from 'yaml';
-import { Authenticator } from '@aws-amplify/ui-react';
+import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react';
 import { generateClient } from "aws-amplify/data";
 import { type Schema } from "@/../amplify/data/resource";
 import { Box, Button, Card, CardContent, Typography } from '@mui/material';
@@ -11,16 +11,23 @@ const amplifyClient = generateClient<Schema>();
 import { withAuth } from '@/components/WithAuth';
 
 const Page = () => {
+    const { user } = useAuthenticator((context) => [context.user]);
     const [chatSessions, setChatSessions] = useState<Schema["ChatSession"]["createType"][]>([]);
 
     useEffect(() => {
         const fetchChatSessions = async () => {
-            const result = await amplifyClient.models.ChatSession.list();
+            const result = await amplifyClient.models.ChatSession.list({
+                filter: {
+                    owner: {
+                        contains: user.userId
+                    }
+                }
+            });
             const sortedChatSessions = result.data.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
             setChatSessions(sortedChatSessions);
         };
         fetchChatSessions();
-    }, []);
+    }, [user.userId]);
 
     return (
         <Authenticator>
