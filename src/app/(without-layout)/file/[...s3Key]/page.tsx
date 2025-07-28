91fee7d368de1352c4cc9ad19a9f238e03e7d578
Walt Mayfield <waltmayf@amazon.com>
2025-07-28 10:22:37 -0500

    cleanup

--- src/app/(without-layout)/file/[...s3Key]/page.tsx
+++ src/app/(without-layout)/file/[...s3Key]/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { NextResponse } from 'next/server';
 import outputs from '@/../amplify_outputs.json';
 import { getUrl } from 'aws-amplify/storage';
 import { Amplify } from 'aws-amplify';
